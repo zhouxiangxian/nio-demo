b54ea47b0924da492ebbb0b86476bba2f7fb0f4f
dell <dell@dell-PC>
2019-02-12 16:36:35 +0800

    pipe管道处理，实现不同线程之间的信息处理

---
+++ src/my/xiaoxian/nio/PipeChannelDemo.java
@@ -0,0 +1,44 @@
+package my.xiaoxian.nio;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Pipe;
+import java.nio.channels.Pipe.SinkChannel;
+import java.nio.channels.Pipe.SourceChannel;
+
+public class PipeChannelDemo {
+	public static void main(String[] args) throws IOException {
+		Pipe open = Pipe.open();
+		new Thread(()->{
+			SourceChannel source = open.source();
+			ByteBuffer buffer = ByteBuffer.allocate(50);
+			try {
+				int read = source.read(buffer);
+				buffer.flip();//重置
+				System.out.println("{接受端}"+new String(buffer.array(),0,read));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		},"接受线程").start();
+		
+		
+		new Thread(()->{
+			String msg="【"+Thread.currentThread()+"www.xiaoxian.com】";
+			SinkChannel sink = open.sink();
+			ByteBuffer buffer = ByteBuffer.allocate(50);
+			buffer.put(msg.getBytes());
+			buffer.flip();
+			while(buffer.hasRemaining()) {
+				try {
+					sink.write(buffer);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			while(buffer.hasRemaining()) {
+				
+			}
+		},"发送线程").start();
+	}
+
+}
